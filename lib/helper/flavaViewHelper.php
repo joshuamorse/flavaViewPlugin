22ab6a53be3c8ea09f0dd3bf7a24ac7cce6f0c68
Joshua Morse <joshua.morse@iostudio.com>
2011-05-27 13:50:16 -0500

    added include_view function to intelligently include a component or partial on the fly

--- lib/helper/flavaViewHelper.php
+++ lib/helper/flavaViewHelper.php
@@ -1,7 +1,7 @@
 <?php
 
 /**
- * include_ajax_view 
+ * EXPERIMENTAL ajax functionality.
  * 
  * @param mixed $type 
  * @param mixed $module_name 
@@ -20,9 +20,73 @@ function include_ajax_view($flava_view_type, $module_name, $view_name, $user_opt
   $options['element_name'] = get_element_name_for($module_name, $view_name);
 
   $options = array_merge($options, $user_options);
-  $options['url'] = url_for('flava_ajax_view_show', $options);
+  $options['url'] = url_for('flava_view_show', $options);
+
+  //$options['options'] = $options;
+  //var_dump($options); die;
 
   include_partial('flavaView/ajax', $options);
+}
+
+/**
+ * Includes a component or partial dependent on a few things:
+ *  - By default, if a component exists, it will be included.
+ *  - By default, if no component exists, a partial will be included.
+ *  - A type (component or partial) can be specified in $view_options. 
+ * 
+ * @param mixed $module_name 
+ * @param mixed $view_name 
+ * @param array $user_options 
+ * @param array $view_options 
+ * @access public
+ * @return void
+ */
+function include_view($module_name, $view_name, $user_options = array(), $view_options = array())
+{
+  if (isset($view_options['type']))
+  {
+    if ($view_options['type'] == 'component')
+    {
+      include_component($module_name, $view_name, $user_options);
+    }
+    else
+    {
+      include_partial($module_name . '/' . $view_name, $user_options);
+    }
+  }
+  else
+  {
+    if (component_exists($module_name, $view_name))
+    {
+      include_component($module_name, $view_name, $user_options);
+    }
+    else
+    {
+      include_partial($module_name . '/' . $view_name, $user_options);
+    }
+  }
+}
+
+/**
+ * Check is a component exists.
+ * 
+ * @param mixed $module_name 
+ * @param mixed $view_name 
+ * @access public
+ * @return void
+ */
+function component_exists($module_name, $view_name)
+{
+  try
+  {
+    get_component($module_name, $view_name);
+  }
+  catch(Exception $e)
+  {
+    return;
+  }
+
+  return true;
 }
 
 /**
@@ -36,14 +100,4 @@ function include_ajax_view($flava_view_type, $module_name, $view_name, $user_opt
 function get_element_name_for($module_name, $view_name)
 {
   return str_replace('_', '-', 'flava-view-' . $module_name . '-' . $view_name);
-}
-
-/**
- * include_view 
- * 
- * @access public
- * @return void
- */
-function include_view()
-{
 }
--- modules/flavaView/templates/_ajax.php
+++ modules/flavaView/templates/_ajax.php
@@ -11,12 +11,21 @@
 */?>
 
 <?php /* The component/partial will be loaded into this element. */ ?>
+
 <div id="<?php echo $element_name ?>" class="flava-ajax-view">
   <?php echo image_tag(sfConfig::get('app_flava_ajax_view_loading')) ?>
 </div>
 
 <script type="text/javascript">
   $(document).ready(function() {
-    $('#<?php echo $element_name ?>').load('<?php echo $sf_data->getRaw('url') ?>');
+    $('#<?php echo $element_name ?>').load('<?php echo $url ?>');
+
+    <?php /*
+    <?php if ($flava_view_type == 'component'): ?>
+      var html = '<?php include_component($module_name, $view_name, $sf_data->getRaw('options')) ?>';
+    <?php elseif ($flava_view_type == 'partial'): ?>
+      var html = "<?php include_partial($module_name . '/' . $view_name, $sf_data->getRaw('options')) ?>";
+    <?php endif ?>
+    */ ?>
   });
 </script>
